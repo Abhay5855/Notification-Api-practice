1332834641c3c490b6505d03c9981b00993a1882
abhay <ap3919529@gmail.com>
2024-02-02 14:28:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("btn");
+const convinceBtn = document.getElementById("convince");
+
+document.addEventListener("DOMContentLoaded", () => {});
+
+//Write your function here
+
+function handleNotification() {}
+
+function handleConvince() {}
+
+btn.addEventListener("click", () => {
+  handleNotification();
+});
+
+convinceBtn.addEventListener("click", () => {
+  handleConvince();
+});
